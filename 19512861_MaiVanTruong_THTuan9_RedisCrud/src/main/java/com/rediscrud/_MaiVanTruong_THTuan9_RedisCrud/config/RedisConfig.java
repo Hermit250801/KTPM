06061a4fe6f423653b5b74b15381874dbe6290bb
Mai Văn Trường <maitruonghandsome@gmail.com>
2022-10-31 13:38:29 +0700

    finish test redis crud

---
+++ 19512861_MaiVanTruong_THTuan9_RedisCrud/src/main/java/com/rediscrud/_MaiVanTruong_THTuan9_RedisCrud/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.rediscrud._MaiVanTruong_THTuan9_RedisCrud.config;
+
+import com.rediscrud._MaiVanTruong_THTuan9_RedisCrud.entity.Employee;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Employee> redisTemplate(){
+        RedisTemplate<String, Employee> empTemplate = new RedisTemplate<>();
+        empTemplate.setConnectionFactory(redisConnectionFactory());
+        return empTemplate;
+    }
+}
---
+++ 19512861_MaiVanTruong_THTuan9_RedisCrud/src/test/java/com/rediscrud/_MaiVanTruong_THTuan9_RedisCrud/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rediscrud._MaiVanTruong_THTuan9_RedisCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
